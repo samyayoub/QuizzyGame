93e3ae501bae310745b8ae8b7328afca406ebd35
Samy Ayoub <samyayoub@gmail.com>
2022-09-21 21:53:35 -0500

    WIP on implementing the Question page and Question card

--- src/Components/QuestionCard.js
+++ src/Components/QuestionCard.js
@@ -1,30 +1,45 @@
 import React from "react"
+import { nanoid } from "nanoid"
 
 export default function QuestionCard(props)  {
 
     //Getting the values of all answers passed on from the props
-    const listOfIncorrectAnswers = props.incorrect_answers
-    const correctAnswer = props.correct_answer
-    const listOfAllAnswers = []
+    
+    // // Function to combine answers
+    // function combineAnswers()   {
+    //     // Loop to combine correct and incorrect answers in one array
+    //     const listOfAllAnswers = []
+    //     for (let i = 0; i < props.incorrect_answers.length; i++)
+    //     {
+    //         listOfAllAnswers.push({
+    //            value: props.incorrect_answers[i],
+    //            isHeld: false,
+    //            id: nanoid()
+    //         })
+    //     }
+    //     // Push the correct answer to the same array
+    //     listOfAllAnswers.push({value: props.correct_answer, isHeld: false, id: nanoid()})
 
-    // Loop to combine correct and incorrect answers in one array
-    // Making each answer a button
-    for (let i = 0; i < listOfIncorrectAnswers.length; i++)
-        {
-            listOfAllAnswers.push(<button className="button-answers">{listOfIncorrectAnswers[i]}</button>)    
-        }
+    //     // Shuffle the array of answers
+    //     listOfAllAnswers.sort(() => Math.random() - 0.5)
 
-    // Push the correct answer to the same array
-    listOfAllAnswers.push(<button className="button-answers">{correctAnswer}</button>)    
+    //     return listOfAllAnswers
+    //     {console.log(listOfAllAnswers)}
+    // }
 
-    // Shuffle the array of answers
-    listOfAllAnswers.sort(() => Math.random() - 0.5)
+    // const answers = props.incorrect_answers.map(answer => {
+    //     <ul>
+    //         <li>{answer.value}</li>
+    //     </ul>
+    // })
 
     return(
         <div>
-            <h4>{props.question}</h4>
-            <h1>{listOfAllAnswers}</h1>
-            <hr className="bottom-line" />
+            <h4>
+                {props.question}
+                {props.incorrect_answers}
+            </h4>
+            <hr />
         </div>
     )
 }
--- src/Components/QuestionPage.js
+++ src/Components/QuestionPage.js
@@ -1,28 +1,62 @@
 import React from "react"
 import QuestionCard from "./QuestionCard"
+import { nanoid } from "nanoid"
 
 export default function QuestionPage()  {
 
     // Setup all states
     const [questionData, setQuestionData] = React.useState([])
-    const [answers, setAnswers] = React.useState([])
+    const [answers, setAnswers] = React.useState(combineAnswers())
     
     // Fetch data
     React.useEffect(() => {
         // console.log(listOfAllQuestions)
-        fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
+        fetch("https://opentdb.com/api.php?amount=10")
             .then(res => res.json())
             .then(data => setQuestionData(data.results))    
     }, [])
 
-    function Answer(props)   {
-        <div>
-            {props}
-        </div>
+    // // Initialize a Lazy State so it will only run component at the beginning
+    // React.useEffect(() => {
+    //     setQuestionData(questionData)    
+    // }, [questionData])
+
+
+    // Function to generate blank question object
+    function generateOneAnswer(props)    {
+        return {
+            value: props.value,
+            isHeld: false,
+            id: nanoid()   
+        }
     }
 
+    // ---------------------------------------------
+    // Function to combine answers
+    function combineAnswers()   {
+        // Loop to combine correct and incorrect answers in one array
+        const listOfAllAnswers = []
+        for (let i = 0; i < questionData.length; i++)
+        {
+            listOfAllAnswers.push(generateOneAnswer(questionData.incorrect_answers[i]))
+        }
+
+        // Push the correct answer to the same array
+        listOfAllAnswers.push({
+            value: questionData.correct_answer,
+            isHeld: false,
+            id: nanoid()
+        })
+
+        // Shuffle the array of answers
+        listOfAllAnswers.sort(() => Math.random() - 0.5)
+
+        return listOfAllAnswers
+    }
+    // ---------------------------------------------
+
     //Getting the values of all answers
-    const listOfIncorrectAnswers = questionData.map(answer => {
+    const allAnswers = answers.map(answer => {
         return <QuestionCard 
             question={answer.question}
             incorrect_answers={answer.incorrect_answers}
@@ -33,13 +67,10 @@ export default function QuestionPage()  {
     // Render all questions and answers
     return(
         <div className="question-page">
-            <ul>
-                {listOfIncorrectAnswers}
-            </ul>
+            {allAnswers}
         </div>
     )
 }
-
 
 
 // {JSON.stringify(questionData, null, 2)}
